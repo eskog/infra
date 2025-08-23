fc01272fdb2ef773216ba994262e31319d747645
Emil Skog <emil@skog.io>
2025-08-23 14:06:42 +0200

    Fix: Corrected bad var-names in script
    Now it aligns with the setenv.sh script

--- packer/Rocky-linux/scripts/delete_template.sh
+++ packer/Rocky-linux/scripts/delete_template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -k -X DELETE \
-  -H "Authorization: PVEAPIToken=$TOKEN_ID=$TOKEN_SECRET" \
+  -H "Authorization: PVEAPIToken=$PKR_VAR_TOKEN_ID=$PKR_VAR_TOKEN_SECRET" \
   "https://$PVEHOST:8006/api2/json/nodes/$PVENODE/qemu/$VMID?purge=1"
